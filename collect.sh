6a6090cfb2a2c9e4c9d4f892b720c574914ac2e3
Michael Oborne <mich146@hotmail.com>
2024-09-02 14:58:08 +1000

    mods

---
+++ collect.sh
@@ -0,0 +1,10 @@
+git diff --ignore-submodules > patches/fw.diff
+cd hdl
+git diff > ../patches/hdl.diff
+cd ../linux
+git diff > ../patches/linux.diff
+cd ../u-boot-xlnx
+git diff > ../patches/u-boot-xlnx.diff
+cd ../buildroot
+git diff > ../patches/buildroot.diff
+cd ../
---
+++ revert.sh
@@ -0,0 +1,10 @@
+git checkout .
+cd hdl
+git checkout .
+cd ../linux
+git checkout .
+cd ../buildroot
+git checkout .
+cd ../u-boot-xlnx
+git checkout .
+cd ../
